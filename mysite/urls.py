fbecf826eacf8a7a7ad2d200a6f6f376e551fdba
bsila <bornasila99@gmail.com>
2023-02-28 06:03:18 +0100

    Fixed categorisation

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import include, path
 
-from newz.views import news_list, scrape
+from newz.views import news_list, resetHeadline, scrape
 
 urlpatterns = [
     path("scrape/", scrape, name="scrape"),
+    path("resetHeadline/", resetHeadline, name="resetHeadline"),
     path("", news_list, name="home"),
     path("admin/", admin.site.urls),
 ]
