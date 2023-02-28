045e9262145ff7010c97a421e57b3f1cedaf9ba1
bsila <bornasila99@gmail.com>
2023-02-28 06:03:18 +0100

    Fixed categorisation

--- newz/views.py
+++ newz/views.py
@@ -10,6 +10,11 @@ from newz.models import Headline, NewsSite
 from .utils import *
 
 
+def resetHeadline(request):
+    Headline.objects.all().delete()
+    return redirect("../")
+
+
 def scrape(request):
     session = requests.Session()
     session.headers = {"User-Agent": "Googlebot/2.1 (+http://www.google.com/bot.html)"}
@@ -22,15 +27,16 @@ def scrape(request):
         News = soup.find_all(newsSite.tag, {"class": newsSite.htmlClass})
 
         for i, article in enumerate(News):
-            print(f"{newsSite.name} article {i}...")
-            data_dir = f"testData/{newsSite.name.lower()}"
-            if not os.path.exists(data_dir):
-                os.mkdir(data_dir)
-            with open(f"{data_dir}/article_{i}.html", "w") as file:
-                file.write(str(article))
 
             main = article if newsSite.name == TPORTAL else article.find_all("a")[0]
-            link = newsSite.hrefPrefix + main["href"]
+            link = (
+                (newsSite.hrefPrefix + main["href"])
+                if not (
+                    str(main["href"]).startswith("http")
+                    or str(main["href"]).startswith("//www")
+                )
+                else str(main["href"])
+            )
             if main.find("img"):
                 image_src = str(
                     main.find("img")["data-src" if newsSite.name == TPORTAL else "src"]
@@ -54,15 +60,33 @@ def scrape(request):
 
             headlines = Headline.objects.filter(url=link, title=title)
             if headlines.count():
+                if headlines.count() > 1:
+                    print(f"More than one")
+                else:
+                    if len(str(link).split("/")) < 4:
+                        print(f"Link to delte: {link}")
+                        headlines.delete()
+                    else:
+                        categorise(headlines.all()[0], newsSite)
                 continue
+
+            print(f"New {newsSite.name} article {i}...")
+            data_dir = f"testData/{newsSite.name.lower()}"
+            if not os.path.exists(data_dir):
+                os.mkdir(data_dir)
+            with open(f"{data_dir}/article_{i}.html", "w") as file:
+                file.write(str(article))
 
             new_headline = Headline()
             new_headline.title = title
             new_headline.url = link
             new_headline.image = image_src
             new_headline.news_site = newsSite
-            categorise(new_headline, newsSite)
-            new_headline.save()
+            if len(str(link).split("/")) < 4:
+                print(f"Invalida link: {link}")
+            else:
+                categorise(new_headline, newsSite)
+                new_headline.save()
     return redirect("../")
 
 
@@ -106,27 +130,87 @@ def news_list(request):
 
 
 def categorise(headline: Headline, newsSite: NewsSite):
-    print("inside categorise")
 
+    try:
+        url = str(str(headline.url).split("/")[3])
+        category = url.replace("/", "")
+    except Exception:
+        print(f"Greška u {headline.url}")
 
-#     url = headline.url.replace(newsSite.hrefPrefix + "/", "")
-#     index = url.find("/", 0)
-#     category = url[0:index]
-#     if category == "sn" or category == "fightsite" or category == "sport":
-#         headline.category = "sport"
-#     elif category == "vijesti" or category == "zagreb":
-#         headline.category = "vijesti"
-#     elif category == "novac":
-#         headline.category = "financije"
-#     elif category == "vaumijau":
-#         headline.category = "ostalo"
-#     elif category == "showbiz" or category == "scena":
-#         headline.category = "showbusiness i scena"
-#     elif category == "lifestyle":
-#         headline.category = "lifestyle"
-#     elif category == "kultura":
-#         headline.category = "kultura"
-#     elif category == "biznis":
-#         headline.category = "business"
-#     elif category == "techsci" or category == "autoklub":
-#         headline.category = "tech i automobili"
+    if (
+        category == "sn"
+        or category == "fightsite"
+        or category == "sport"
+        or category == "fit"
+    ):
+        headline.category = "sport"
+    elif (
+        category == "vijesti"
+        or category == "zagreb"
+        or category == "videovijesti"
+        or category == "vijest"
+        or category == "danas"
+        or category == "hrvatska"
+        or category == "regija"
+        or category == "aktualno"
+        or category == "eu"
+        or category == "svijet"
+        or category == "manjine"
+        or category == "video"
+    ):
+        headline.category = "vijesti"
+    elif category == "novac" or category == "gospodarstvo":
+        headline.category = "financije"
+    elif category == "vaumijau" or category == "native" or category == "webcafe":
+        headline.category = "ostalo"
+    elif (
+        category == "showbiz"
+        or category == "scena"
+        or category == "show"
+        or category == "showtime"
+        or category == "magazi"
+        or category == "magazin"
+        or category == "hot"
+        or category == "super1"
+        or category == "zvijezde"
+    ):
+        headline.category = "showbusiness i scena"
+    elif (
+        category == "lifestyle"
+        or category == "chill"
+        or category == "shopping"
+        or category == "zivot"
+        or category == "pitanje-zdravlja"
+        or category == "mame"
+        or category == "food"
+        or category == "bolja-ja"
+    ):
+        headline.category = "lifestyle"
+    elif category == "moda" or category == "ljepota":
+        headline.category = "moda"
+    elif category == "kultura":
+        headline.category = "kultura"
+    elif category == "biznis":
+        headline.category = "business"
+    elif (
+        category == "techsci"
+        or category == "autoklub"
+        or category == "autozona"
+        or category == "techno"
+        or category == "auto"
+        or category == "tehno"
+    ):
+        headline.category = "tech i automobili"
+    elif category == "crna-kronika" or category == "politika-kriminal":
+        headline.category = "crna kronika"
+    elif category == "horoskop" or category == "astro":
+        headline.category = "horoskop"
+    elif category == "promo":
+        headline.category = "promo"
+    elif category == "infokutak" or category == "znanost" or category == "edukacija":
+        headline.category = "info i znanost"
+    else:
+        headline.category = "nekategorizirano"
+        print(f"new category: {category} - link: {headline.url}")
+
+    headline.save()
